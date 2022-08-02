06a1afb890720c7f7ffc0d4bc821d48a9e22b0ea
Bryan Bosire <bryanbosire@gmail.com>
2022-08-02 12:53:20 +0300

    commit tree f747c4df1fe2d9a702327e29e172fbdaf75dc86d

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewTaskForm() {
+function NewTaskForm({categories, onAddTasks}) {
+  const [inputValue, setInputValue] = useState({
+    text: "",
+    category: "",
+  });
+
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+    setInputValue({...inputValue, [name]: value});
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onAddTasks(inputValue);
+  }
+
+
   return (
-    <form className="new-task-form">
+    <form onSubmit={handleSubmit} className="new-task-form">
       <label>
         Details
-        <input type="text" name="text" />
+        <input onChange={handleChange} type="text" name="text" />
       </label>
       <label>
         Category
-        <select name="category">
+        <select onChange={handleChange} name="category">
           {/* render <option> elements for each category here */}
+          {categories.map((category, index) => {
+            if(category !== "All") {
+              return (
+                <option key={index} value={category}>
+                  {category}
+                </option>
+              )
+            } 
+          })}
         </select>
       </label>
       <input type="submit" value="Add task" />
